720ad50ef61904998aadfd5130764ba07708757c
Albert Koy <albertkoy@gmail.com>
2014-12-23 18:29:49 -0500

    initial trend line code

--- init.go
+++ init.go
@@ -15,10 +15,15 @@ const (
 	// symbol, month, day, year, month, day, year
 	YAHOO_FINANCE_API_URL string = "http://real-chart.finance.yahoo.com/table.csv?s=%s&d=%s&e=%s&f=%s&g=d&a=%s&b=%s&c=%s&ignore=.csv"
 	STOCK_FILE            string = "/Users/albert/Desktop/stocks/stocks_sample.txt"
-	NUM_YEARS_DATA        int    = 3
+	NUM_YEARS_DATA        int    = 1
 	START_PIVOT_WIDTH     int    = 3
 	PIVOT_WIDTH           int    = 5
 )
+
+type Line struct {
+	X1, Y1, X2, Y2 float64
+	slope() float64
+}
 
 type StockData struct {
 	Data   []StockBar
@@ -35,12 +40,16 @@ type StockBar struct {
 	AdjClose float64
 }
 
+func slope(l* Line) float64 {
+	return (l.Y2 - l.Y1) / (l.X2 - l.X1)
+}
+
 func main() {
 	t := time.Now()
 	month := fmt.Sprintf("%02d", t.Month()-1)
 	itoa := strconv.Itoa
 
-	var c chan StockData = make(chan StockData, 1)
+	var c chan StockData = make(chan StockData, 15)
 
 	file, err := os.Open(STOCK_FILE)
 	if err != nil {
@@ -58,33 +67,98 @@ func main() {
 		log.Fatal(err)
 	}
 
-	for i := 0; i < 6000; i++ {
-		data := <-c
-		// if data != nil {
-			fmt.Println(data.Symbol)
-			for i := range getPivots(data.Data, false, PIVOT_WIDTH) {
-				fmt.Println(data.Data[i].Date)
-			}
-		// }
+	for i := 0; i < 15; i++ {
+		stock := <-c
+		fmt.Println(stock.Symbol)
+		for _, pivotIndex := range getStartPivots(stock, false) {
+			fmt.Println(stock.Data[pivotIndex].Date)
+		}
 	}
 
 	var input string
 	fmt.Scanln(&input)
 }
 
-func getPivots(data []StockBar, getHighPivots bool, width int) []int {
-	var pivots []int
+func getLines(stock StockData, startPivots []int, pivots []int, getHighLines bool) []Line, []Line, []Line {
+	var lines []Line
+	currentPivotIndex := 0
 
-	for i, value := range data[width:-width] {
+	for i, startPivot := range startPivots {
+		var prevLine Line
+		prevLine = nil
+		for j := currentPivotIndex; j < len(pivots); j++ {
+			pivot := pivots[j]
+			if pivot > startPivot && pivots[currentPivotIndex] <= startPivot {
+				currentPivotIndex = i
+			}
+			if pivot > startPivot {
+				if getHighLines {
+					currLine := Line(startPivot, stock.Data[startPivot].High, pivot, stock.Data[pivot].High)
+					if prevLine == nil || currLine.slope() >= prevLine.slope() {
+						prevLine = currLine
+						lines = append(lines, prevLine)
+					}
+				} else {
+					currLine := Line(startPivot, stock.Data[startPivot].Low, pivot, stock.Data[pivot].Low)
+					if prevLine == nil || currLine.slope() <= prevLine.slope() {
+						prevLine = currLine
+						lines = append(lines, prevLine)
+					}
+				}
+			}
+		}
+	}
+
+	var trendLines []Line
+	var trendChannelLines []Line
+	var horizontalLines []Line
+
+	for _, line := range lines {
+		if getHighLines {
+			if line.slope() > 0 {
+				trendChannelLines = append(trendChannelLines, line)
+			} else if line.slope() < 0 {
+				trendLines = append(trendLines, line)
+			} else {
+				horizontalLines = append(horizontalLines, line)
+			}
+		} else {
+			if line.slope() > 0 {
+				trendLines = append(trendLines, line)
+			} else if line.slope() < 0 {
+				trendChannelLines = append(trendChannelLines, line)
+			} else {
+				horizontalLines = append(horizontalLines, line)
+			}
+		}
+	}
+
+	return trendLines, trendChannelLines, horizontalLines
+}
+
+// draw lines for low pivots:
+// iteratively go through pivots
+// have an anchor pivot (use start pivots as anchor pivots)
+// create a line if a next pivot creates a line with a slope less than what's been seen so far
+
+func getStartPivots(stock StockData, getHighPivots bool) []int {
+	return getPivots(stock, getHighPivots, START_PIVOT_WIDTH)
+}
+
+func getPivots(stock StockData, getHighPivots bool, width int) []int {
+	var pivots []int
+	data := stock.Data
+
+	for i := width; i < len(data)-width; i++ {
 		isPivot := true
 		for j := i - width; j <= i+width; j++ {
 			if getHighPivots {
-				if data[j].High > value.High {
+				if data[j].High > data[i].High {
 					isPivot = false
 					break
 				}
 			} else {
-				if data[j].Low < value.Low {
+				if data[j].Low < data[i].Low {
 					isPivot = false
 					break
 				}
@@ -102,7 +176,6 @@ func getStockData(c chan StockData, symbol, endMonth, endDay, endYear, startMont
 	url := fmt.Sprintf(YAHOO_FINANCE_API_URL, symbol, endMonth, endDay, endYear, startMonth, startDay, startYear)
 	resp, httpErr := http.Get(url)
 	if httpErr != nil {
-		// c <- nil
 		return
 	}
 	defer resp.Body.Close()
@@ -111,7 +184,6 @@ func getStockData(c chan StockData, symbol, endMonth, endDay, endYear, startMont
 	rawCSVdata, csvErr := reader.ReadAll()
 
 	if csvErr != nil {
-		// c <- nil
 		return
 	}
 
