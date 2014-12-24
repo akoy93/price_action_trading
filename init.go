0dcde267b45b284808f5de4c57549a54f4851c14
Albert Koy <albertkoy@gmail.com>
2014-12-23 19:42:15 -0500

    tl/tcl identification works

--- init.go
+++ init.go
@@ -18,11 +18,14 @@ const (
 	NUM_YEARS_DATA        int    = 1
 	START_PIVOT_WIDTH     int    = 3
 	PIVOT_WIDTH           int    = 5
+	HORIZONTAL_THRESHOLD  float64 = 0.01
 )
 
 type Line struct {
-	X1, Y1, X2, Y2 float64
-	slope() float64
+	X1 int
+	Y1 float64
+	X2 int
+	Y2 float64
 }
 
 type StockData struct {
@@ -40,8 +43,8 @@ type StockBar struct {
 	AdjClose float64
 }
 
-func slope(l* Line) float64 {
-	return (l.Y2 - l.Y1) / (l.X2 - l.X1)
+func (l* Line) Slope() float64 {
+	return (l.Y2 - l.Y1) / float64(l.X2 - l.X1)
 }
 
 func main() {
@@ -70,39 +73,68 @@ func main() {
 	for i := 0; i < 15; i++ {
 		stock := <-c
 		fmt.Println(stock.Symbol)
-		for _, pivotIndex := range getStartPivots(stock, false) {
-			fmt.Println(stock.Data[pivotIndex].Date)
+		trendLines, trendChannelLines, horizontalLines := getLines(stock, false)
+		fmt.Println(len(trendLines), len(trendChannelLines), len(horizontalLines))
+		for _, line := range trendChannelLines {
+			fmt.Println(line.X1, stock.Data[line.X1].Date)
+			fmt.Println(line.X2, stock.Data[line.X2].Date)
+			fmt.Println(line.Y1, line.Y2)
+			fmt.Println(line.Slope())
+			fmt.Println()
 		}
+
+		// for _, pivot := range getPivots(stock, false, PIVOT_WIDTH) {
+		// 	fmt.Println(stock.Data[pivot].Date)
+		// }
+		// for _, pivotIndex := range getStartPivots(stock, false) {
+		// 	fmt.Println(stock.Data[pivotIndex].Date)
+		// }
 	}
 
 	var input string
 	fmt.Scanln(&input)
 }
 
-func getLines(stock StockData, startPivots []int, pivots []int, getHighLines bool) []Line, []Line, []Line {
+// returns trendLines, trendChannelLines, horizontalLines
+func getLines(stock StockData, getOverLines bool) ([]Line, []Line, []Line) {
+	startPivots := getStartPivots(stock, getOverLines)
+	endPivots := getPivots(stock, getOverLines, PIVOT_WIDTH)
+	return getLinesFromPivots(stock, startPivots, endPivots, getOverLines)
+}
+
+// add checks for empty pivots
+func getLinesFromPivots(stock StockData, startPivots []int, pivots []int, getHighLines bool) ([]Line, []Line, []Line) {
 	var lines []Line
 	currentPivotIndex := 0
 
-	for i, startPivot := range startPivots {
-		var prevLine Line
+	for _, startPivot := range startPivots {
+		var prevLine interface{}
 		prevLine = nil
 		for j := currentPivotIndex; j < len(pivots); j++ {
 			pivot := pivots[j]
-			if pivot > startPivot && pivots[currentPivotIndex] <= startPivot {
-				currentPivotIndex = i
-			}
 			if pivot > startPivot {
+				if pivots[currentPivotIndex] <= startPivot {
+					currentPivotIndex = j
+				}
+
+				// interface conversion
+				var prevLineConverted Line
+				if prevLine != nil {
+					prevLineConverted = prevLine.(Line)
+				}
+
+				// draw lines
 				if getHighLines {
-					currLine := Line(startPivot, stock.Data[startPivot].High, pivot, stock.Data[pivot].High)
-					if prevLine == nil || currLine.slope() >= prevLine.slope() {
+					currLine := Line{startPivot, stock.Data[startPivot].High, pivot, stock.Data[pivot].High}
+					if prevLine == nil || currLine.Slope() >= prevLineConverted.Slope() {
 						prevLine = currLine
-						lines = append(lines, prevLine)
+						lines = append(lines, currLine)
 					}
 				} else {
-					currLine := Line(startPivot, stock.Data[startPivot].Low, pivot, stock.Data[pivot].Low)
-					if prevLine == nil || currLine.slope() <= prevLine.slope() {
+					currLine := Line{startPivot, stock.Data[startPivot].Low, pivot, stock.Data[pivot].Low}
+					if prevLine == nil || currLine.Slope() <= prevLineConverted.Slope() {
 						prevLine = currLine
-						lines = append(lines, prevLine)
+						lines = append(lines, currLine)
 					}
 				}
 			}
@@ -115,17 +147,17 @@ func getLines(stock StockData, startPivots []int, pivots []int, getHighLines boo
 
 	for _, line := range lines {
 		if getHighLines {
-			if line.slope() > 0 {
+			if line.Slope() > HORIZONTAL_THRESHOLD {
 				trendChannelLines = append(trendChannelLines, line)
-			} else if line.slope() < 0 {
+			} else if line.Slope() < -HORIZONTAL_THRESHOLD {
 				trendLines = append(trendLines, line)
 			} else {
 				horizontalLines = append(horizontalLines, line)
 			}
 		} else {
-			if line.slope() > 0 {
+			if line.Slope() > HORIZONTAL_THRESHOLD {
 				trendLines = append(trendLines, line)
-			} else if line.slope() < 0 {
+			} else if line.Slope() < -HORIZONTAL_THRESHOLD {
 				trendChannelLines = append(trendChannelLines, line)
 			} else {
 				horizontalLines = append(horizontalLines, line)
